29c30377c3c726b8281ffb218f8d5687455b2794
Cheesedz <chisnghia@gmail.com>
2024-05-10 13:42:21 +0700

    refactor: user service query

--- core/model/src/main/kotlin/com/example/model/UserData.kt
+++ core/model/src/main/kotlin/com/example/model/UserData.kt
@@ -1,13 +1,13 @@
 package com.example.model
 
-data class UserData (
-    val id: String,
-    val name: String,
-    val email: String,
-    val image: Int? = null,
-    val joinedServers : List<String>,
-    val directMessages: List<String>,
-    val friends: List<UserData>
-    // val status: String
-    // val about: String
+data class UserData(
+    var id: String? = null,
+    var username: String,
+    var password: String,
+    var email: String,
+    var image: Int? = null,
+    var joinedServers: List<String>,
+    var friends: List<String>,
+    // var status: String
+//     var about: String? = null
 )
