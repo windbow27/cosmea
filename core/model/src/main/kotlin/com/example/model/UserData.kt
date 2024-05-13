1e3301e090488c5f19cfe9d0d3a7528de1fad042
Cheesedz <chisnghia@gmail.com>
2024-05-13 11:01:25 +0700

    feature: add category into server

--- core/model/src/main/kotlin/com/example/model/UserData.kt
+++ core/model/src/main/kotlin/com/example/model/UserData.kt
@@ -4,7 +4,6 @@ class UserData(
     var username: String,
     var password: String,
     var email: String,
-    var image: List<String>? = null,
     var joinedServers: List<String>,
     var friends: List<String>,
     var profile: ProfileData? = null,
