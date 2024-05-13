5f90a04a212c1611a3664c394b4342de6dd2875d
Cheesedz <chisnghia@gmail.com>
2024-05-13 10:49:38 +0700

    feature: auth service & email check

--- core/model/src/main/kotlin/com/example/model/UserData.kt
+++ core/model/src/main/kotlin/com/example/model/UserData.kt
@@ -1,14 +1,14 @@
 package com.example.model
 
+import com.example.model.generateUserId
+
 class UserData(
-    var id: String? = null,
     var username: String,
     var password: String,
     var email: String,
-    var image: Int? = null,
+    var image: String? = null,
     var joinedServers: MutableList<String>,
     var friends: MutableList<String>,
-    var profile: ProfileData? = null
-    // var status: String
-//     var about: String? = null
+    var profile: ProfileData? = null,
+    var id: String? = generateUserId(username),
 )
